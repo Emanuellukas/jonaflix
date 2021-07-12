3886001fe3b26f0b35db06d025defdea13ce4e04
Lucas Emanuel <lucasesn23@gmail.com>
2021-07-11 22:12:21 -0300

    update nuxt

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>puta vida</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Jonaflix'
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Blog pessoal e opinativo sobre obras em geral da cultura pop.'
+        }
+      ]
+    }
+  }
+}
+</script>
